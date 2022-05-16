0b292f932e84e9dad8f5fa516cf4e2dc80fba45e
Michael Dornhof <michael.dornhof@haw-hamburg.de>
2022-05-16 16:56:37 +0200

    ADDED: (not working) solution

--- src/haw/aydosch/Blatt1/A3/SecureFile.java
+++ src/haw/aydosch/Blatt1/A3/SecureFile.java
@@ -1,131 +1,113 @@
 package haw.aydosch.Blatt1.A3;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class SecureFile {
     public static void main(String[] args) {
+        // Parse arguments
         String fileEnDecryptFile = args[0];
         String fileKey = args[1];
         String resultFile = args[2];
         String operation = args[3];
 
-        // Read Keys from Key-File
+        // Prepare TripleDES relevant data
         byte[] key1 = new byte[8];
         byte[] key2 = new byte[8];
         byte[] key3 = new byte[8];
         byte[] iv = new byte[8];
-        try {
-            byte[] bytesKeyFile = Files.readAllBytes(Paths.get(fileKey));
-            key1 = Arrays.copyOfRange(bytesKeyFile, 0, 8);
-            key2 = Arrays.copyOfRange(bytesKeyFile, 8, 16);
-            key3 = Arrays.copyOfRange(bytesKeyFile, 16, 24);
-            iv = Arrays.copyOfRange(bytesKeyFile, 24, 32);
 
-        } catch (IOException exception) {
+        // Prepare file arrays
+        byte[] src = new byte[0];
+        byte[] target = new byte[0];
+
+        try {
+            // Read Source File
+            src = Files.readAllBytes(Paths.get(fileEnDecryptFile));
+
+            // Create target array using source file length
+            target = new byte[src.length];
+
+            // Read key file and extract relevant data
+            byte[] keyfile = Files.readAllBytes(Paths.get(fileKey));
+            key1 = Arrays.copyOfRange(keyfile, 0, 8);
+            key2 = Arrays.copyOfRange(keyfile, 8, 16);
+            key3 = Arrays.copyOfRange(keyfile, 16, 24);
+            iv = Arrays.copyOfRange(keyfile, 24, 32);
+        } catch (Exception exception) {
             exception.printStackTrace();
             System.exit(-1);
         }
 
-        System.out.println(key1.length);
-
+        // create TripleDES Object
         TripleDES tdes = new TripleDES(key1, key2, key3);
 
-        byte[] result;
-        if(operation.equals("encrypt"))
-            result = SecureFile.encrypt(tdes, iv, fileEnDecryptFile);
-        else
-            result = SecureFile.decrypt(tdes, iv, fileEnDecryptFile);
+        // Perform operation
+        if(operation.equals("encrypt")) SecureFile.encrypt(tdes, iv, src, target);
+        if(operation.equals("decrypt")) SecureFile.decrypt(tdes, iv, src, target);
 
-        try {
-            FileOutputStream out = new FileOutputStream(resultFile);
-            out.write(result);
+        // Write target to result file
+        try (FileOutputStream fos = new FileOutputStream(resultFile)) {
+            fos.write(target);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static byte[] encrypt(TripleDES tdes, byte[] iv, String toEncrypt) {
-        byte[] target = new byte[0];
+    private static byte[] encrypt(TripleDES tdes, byte[] iv, byte[] src, byte[] target) {
+        byte[] prev_c = iv;
 
-        try {
-            byte[] sourceFile = Files.readAllBytes(Paths.get(toEncrypt));
-            byte[] cipher = tdes.encryptBytes(iv);
+        for(int i = 0; i < src.length; i+=8) {
+            // Get 8 bytes of plaintext
+            byte[] m_i = Arrays.copyOfRange(src, i, i+8);
 
-            int offset = 0;
-            byte[] plain;
-            byte[] curr;
-            target = new byte[sourceFile.length];
+            // Calculate chiffretext
+            byte[] result_c = xor(m_i, tdes.encryptBytes(prev_c));
 
-            // Erhöhe nach jedem Durchlauf den Offset um 8 Bytes
-            for (int x = 0; x < sourceFile.length; x += 8) {
-                if (x + 8 > sourceFile.length - 1) {
-                    offset = sourceFile.length;
-                }
-                else {
-                    offset = x + 8;
-                }
+            // Update prev_c
+            prev_c = result_c;
 
-                // Befülle den PlaintextArray
-                plain = Arrays.copyOfRange(target, x, offset);
+            // Append to target
+            for(int j = 0; j < 8; j++) {
+                // Break if target length is reached (Everything above length is going to be padding)
+                if(i+j == src.length)
+                    break;
 
-                // CFB XOR
-                curr = xor(cipher, plain);
-                System.arraycopy(curr, 0, target, x, offset - x);
-                cipher = tdes.encryptBytes(curr);
+                target[i+j] = result_c[j];
             }
-        } catch (IOException exception) {
-            exception.printStackTrace();
         }
 
         return target;
     }
 
-    private static byte[] decrypt(TripleDES tdes, byte[] iv, String toEncrypt) {
-        byte[] target = new byte[0];
+    private static byte[] decrypt(TripleDES tdes, byte[] iv, byte[] src, byte[] target) {
+        byte[] prev_c = iv;
 
-        try {
-            byte[] sourceFile = Files.readAllBytes(Paths.get(toEncrypt));
-            target = new byte[sourceFile.length];
+        for(int i = 0; i < src.length; i+=8) {
+            // Get 8 bytes of plaintext
+            byte[] m_i = Arrays.copyOfRange(src, i, i+8);
 
+            // Calculate chiffretext
+            byte[] result_c = xor(m_i, tdes.decryptBytes(prev_c));
 
-            byte[] curr = tdes.decryptBytes(iv);
+            // Update prev_c
+            prev_c = result_c;
 
-            // Plaintext buffer
-            byte[] plain = new byte[8];
+            // Append to target
+            for(int j = 0; j < 8; j++) {
+                // Break if target length is reached (Everything above length is going to be padding)
+                if(i+j == target.length)
+                    break;
 
-            // Chiffre buffer
-            byte[] cipher = new byte[8];
-            int offset = 0;
-
-            // Erhöhe nach jedem Durchlauf den Offset um 8 Bytes
-            for (int x = 0; x < sourceFile.length; x += 8) {
-                if (x + 8 > sourceFile.length - 1) {
-                    offset = sourceFile.length;
-                }
-                else {
-                    offset = x + 8;
-                }
-
-                cipher = Arrays.copyOfRange(target, x, offset);
-
-                // CFB XOR
-                plain = xor(curr, cipher);
-
-                System.arraycopy(plain, 0, target, x, offset - x);
-                curr = tdes.encryptBytes(cipher);
+                target[i+j] = result_c[j];
             }
-        } catch (IOException exception) {
-            exception.printStackTrace();
         }
 
         return target;
     }
+
 
 
     public static byte[] xor(byte[] o1, byte[] o2) {
